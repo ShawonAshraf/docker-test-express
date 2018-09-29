8b40d470d3998fd2d837da30adf408da07c9beda
Shawon Ashraf <shawon13@live.com>
2018-09-30 03:59:11 +0600

    port change on server.js

--- src/server.js
+++ src/server.js
@@ -20,7 +20,7 @@ app.get('/status', (req, res) => {
   });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running at * ${port}`);
 });
